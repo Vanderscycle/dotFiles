24ec55b640bfa15a538c58a1652a0b832e2ed496
Henri Vandersleyen <h52706578+Vanderscycle@users.noreply.github.com>
2021-03-14 14:02:43 -0700

    small changes to the post install script

--- postInstallwork.sh
+++ postInstallwork.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 # example https://gist.github.com/rougeth/8108714
+# wget https://raw.githubusercontent.com/Vanderscycle/ubuntu-dot-config/main/postInstallwork.sh && chmod +x postInstallwork.sh && bash postInstallwork.sh
 # Ubuntu post-install script
 echo '------------------------------------------------------------------------'
 echo '=> Ubuntu 20.04LTS post-install script'
 echo '------------------------------------------------------------------------'
 
 
-echo '=> Update repository information'
+echo -e '\n=> Update repository information '
 sudo apt-get update -qq #-qq for quiet
-echo '=> Performe system upgrade'
+echo -e '=> Perform system upgrade'
 sudo apt-get dist-upgrade -y
-echo 'Done.'
+echo -e 'Done.\n'
 # -----------------------------------------------------------------------------
 # => Install system utilities
 # -----------------------------------------------------------------------------
 
-echo '=> Install system utilities'
+echo -e '\n=> Install system utilities'
 sudo apt-get install -y --no-install-recommends curl wget git lsof gdebi-core \
     zip unzip gzip tar \
     ssh
-echo 'Done.'
-# -----------------------------------------------------------------------------
-# => Install Terminal Specific
-# -----------------------------------------------------------------------------
-echo '=> Installing zsh and theme'
-sudo apt-get install -y --no-install-recommends zsh
-# changing the default from bash to zsh
-chsh -s $(which zsh) 
-# installing oh-my-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-echo 'Done.'
+echo -e 'Done.\n'
 
 # -----------------------------------------------------------------------------
 # => Install developer packages
 # -----------------------------------------------------------------------------
 
-echo '=> Install developer packages'
+echo -e '\n=> Install developer packages'
 sudo apt-get install -y --no-install-recommends git neovim python3-pip
-echo 'Done.'
+echo -e 'Done.\n'
 
 # -----------------------------------------------------------------------------
 # => Docker (debian)
@@ -48,15 +37,15 @@ echo 'Done.'
 
 echo '=> Removing old docker version (if present) Docker' 
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
-echo '=> Installing Docker' 
+echo -e'\n=> Installing Docker' 
 
 sudo apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
 apt-cache madison docker-ce
 sudo apt-get install docker-ce=5:20.10.5~3-0~ubuntu-bionic docker-ce-cli=5:20.10.5~3-0~ubuntu-bionic containerd.io
-echo '=> Installing Docker-compose' 
+echo e'\n=> Installing Docker-compose' 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-echo 'Done.'
+echo -e 'Done.\n'
 
 # -----------------------------------------------------------------------------
 # => Miniconda
@@ -68,18 +57,18 @@ bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
 conda create -n base python
 conda install pandas numpy django
-echo 'Done.'
+echo -e 'Done.\n'
 
 # -----------------------------------------------------------------------------
 # => Databases
 # -----------------------------------------------------------------------------
 
-echo '=> Installing Postgres'
+echo -e '\n=> Installing Postgres'
 sudo apt-get install -y --no-install-recommends postgresql postgresql-contrib
 echo '=> Starting Postgres'
 sudo systemctl enable postgresql
 service postgresql start
-echo '=> Installing MongoDb'
+echo -e '\n=> Installing MongoDb'
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
@@ -87,7 +76,7 @@ sudo apt-get install -y --no-install-recommends mongodb-org
 echo '=> Starting Mongo'
 sudo systemctl enable mongod
 sudo systemctl start mongod
-echo 'Done.'
+echo -e 'Done. \n'
 
 # -----------------------------------------------------------------------------
 # => Security
@@ -97,7 +86,7 @@ echo '=> Configuring security'
 mkdir -p ~/.ssh
 sudo chmod 700 ~/.ssh/
 sudo chmod 600 ~/.ssh/*
-echo 'Done.'
+echo -e 'Done.\n'
 
 # -----------------------------------------------------------------------------
 # => Get dotfiles
@@ -114,13 +103,13 @@ cd "$dotfiles_path"
 # Copy all dotfiles except .git/ and .gitmodules
 cp -r `ls -d .??* | egrep -v '(.git$|.gitmodules)'` $HOME
 
-echo 'Done.'
+echo -e 'Done.\n'
 
 
 # -----------------------------------------------------------------------------
 # => if local machine
 # -----------------------------------------------------------------------------
-echo '=> Install favorite applications'
+echo '\n=> Install favorite applications'
 echo '=> spotify discord mailspring vlc vscode gitkraken'
 echo -e '=> Are you sure? [Y/n] '
 read confirmation
@@ -133,5 +122,18 @@ if [[ $confirmation == 'YES' || $confirmation == 'Y' ]]; then
     sudo dpkg -i gitkraken-amd64.deb
 
 
-    echo 'Done.'
+    echo -e 'Done.\n'
 fi
+
+# -----------------------------------------------------------------------------
+# => Install Terminal Specific
+# -----------------------------------------------------------------------------
+echo -e '\n=> Installing zsh and theme'
+sudo apt-get install -y --no-install-recommends zsh
+# changing the default from bash to zsh
+chsh -s $(which zsh) -y
+# installing oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo -e 'Done.\n'
