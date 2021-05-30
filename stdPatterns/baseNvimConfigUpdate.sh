492a18cb445131074d784ca9d213456a44c098d3
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-05-30 16:16:52 -0700

    trying to use block comments

--- stdPatterns/baseNvimConfigUpdate.sh
+++ stdPatterns/baseNvimConfigUpdate.sh
@@ -4,3 +4,14 @@ git clone https://github.com/siduck76/chad-nvim.git ~/tempDoots/
 rsync -auvn ~/tempDoots/lua/ ~/.config/nvim/lua/ --exclude mappings/ --exclude pluginsList/ --exclude misc-utils/ --exclude lspconfig/
 # post install clean-up
 rm -rf ~/tempDoots
+
+# folders that I updated
+<<COMMENT
+    treesitter
+    lsp-config
+    plugins
+    dashboard (addition)
+    mappings
+    misc-utils
+COMMENT
+#
