0b13a60b230bbc1675e03ca35a21720eeaf3e394
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-06-24 12:16:19 -0700

    need to refactor all the loose highlight ends into highlights.lua

---
+++ nvim/lua/themes/nord.lua
@@ -0,0 +1,20 @@
+local colors = {
+    BASE00="#2e3440",
+    BASE01="#3b4252",
+    BASE02="#434c5e",
+    BASE03="#4c566a",
+    BASE04="#d8dee9",
+    BASE05="#e5e9f0",
+    BASE06="#eceff4",
+    BASE07="#8fbcbb",
+    BASE08="#88c0d0",
+    BASE09="#81a1c1",
+    BASE0a="#5e81ac",
+    BASE0b="#bf616a",
+    BASE0c="#d08770",
+    BASE0d="#ebcb8b",
+    BASE0e="#a3be8c",
+    BASE0f="#b48ead",
+}
+
+return colors
