114edaa6025a67f1768d0ba14ef13c88d2d313cf
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-06-28 09:07:26 -0700

    added a custom area that shows errors.

--- nvim/lua/top-bufferline.lua
+++ nvim/lua/top-bufferline.lua
@@ -14,6 +14,32 @@ require "bufferline".setup {
         view = "multiwindow",
         show_buffer_close_icons = true,
         separator_style = "thin",
-        mappings = "true"
+        mappings = "true",
+        custom_areas = {
+            right = function()
+                local result = {}
+                local error = vim.lsp.diagnostic.get_count(0, [[Error]])
+                local warning = vim.lsp.diagnostic.get_count(0, [[Warning]])
+                local info = vim.lsp.diagnostic.get_count(0, [[Information]])
+                local hint = vim.lsp.diagnostic.get_count(0, [[Hint]])
+
+                if error ~= 0 then
+                result[1] = {text = "  " .. error, guifg = "#EC5241"}
+                end
+
+                if warning ~= 0 then
+                result[2] = {text = "  " .. warning, guifg = "#EFB839"}
+                end
+
+                if hint ~= 0 then
+                result[3] = {text = "  " .. hint, guifg = "#A3BA5E"}
+                end
+
+                if info ~= 0 then
+                result[4] = {text = "  " .. info, guifg = "#7EA9A7"}
+            end
+            return result
+        end
+    }
     }
 }
