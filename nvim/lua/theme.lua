926787ff5cf6cbd228ee097246c7bc76f45e7b3f
Henri <52706578+Vanderscycle@users.noreply.github.com>
2021-08-02 09:27:21 -0700

    Delete nvim directory

--- nvim/lua/theme.lua
+++
@@ -1,7 +0,0 @@
-g = vim.g
-local base16 = require "base16"
---base16(base16.themes["onedark"], true)
-g.nvchad_theme = "onedark"
--- base16(base16.themes["tomorrow-night-eighties"], true)
-base16(base16.themes["nord"], true)
-
