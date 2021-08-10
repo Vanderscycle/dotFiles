a7d441fbe89c273c5130e9c6f51ab5a7595cdaa0
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-08-10 10:21:43 -0700

    final changes

--- nvim/lua/pluginList.lua
+++ nvim/lua/pluginList.lua
@@ -328,10 +328,34 @@ return packer.startup(
                 }
             end
         }
-        --TODO: make it lazy load
+        use {
+          'abecodes/tabout.nvim',
+          config = function()
+            require('tabout').setup {
+            tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+            backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+            act_as_tab = true, -- shift content if tab out is not possible
+            act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+            enable_backwards = true, -- well ...
+            completion = true, -- if the tabkey is used in a completion pum
+            tabouts = {
+              {open = "'", close = "'"},
+              {open = '"', close = '"'},
+              {open = '`', close = '`'},
+              {open = '(', close = ')'},
+              {open = '[', close = ']'},
+              {open = '{', close = '}'}
+            },
+            ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+            exclude = {} -- tabout will ignore these filetypes
+        }
+          end,
+                wants = {'nvim-treesitter'}, -- or require if not used so far
+                after = {'nvim-compe'} -- if a completion plugin is using tabs load it before
+        }
+        --TODO: make it lazy load (both)
         use "Pocco81/DAPInstall.nvim"
         use { --WARN: need to integrate
-              --TODO: make it lazy load
               -- TODO: add bindings
             'mfussenegger/nvim-dap',
         }
