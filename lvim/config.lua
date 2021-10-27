decb8e8bd5526e4d39c9468d4fe8f5643bb56c27
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-10-27 14:33:01 -0700

    fixed the annoying lua issue

--- lvim/config.lua
+++ lvim/config.lua
@@ -18,17 +18,20 @@ lvim.keys.normal_mode["<A-t>"] = ":ToggleTerm<cr>"
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
-lvim.builtin.telescope.on_config_done = function()
   local actions = require "telescope.actions"
-  -- for input mode
-  lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
-  lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
-  lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
-  lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
-  -- for normal mode
-  lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
-  lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
-end
+  lvim.builtin.telescope.defaults.mappings = {
+      i = {
+     ["<C-j>"] = actions.move_selection_next,
+     ["<C-k>"] = actions.move_selection_previous,
+     ["<C-n>"] = actions.cycle_history_next,
+     ["<C-p>"] = actions.cycle_history_prev,
+   },
+   n = {
+     ["<C-j>"] = actions.move_selection_next,
+     ["<C-k>"] = actions.move_selection_previous,
+   },
+ }
+
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -90,7 +93,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- set an additional linter
 -- lvim.lang.python.linters = {
 --   {
---     exe = "flake8",
+--     exe = "flake",
 --     args = {}
 --   }
 -- }
