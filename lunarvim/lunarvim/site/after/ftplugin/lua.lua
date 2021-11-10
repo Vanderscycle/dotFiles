6fc1ae7439d4dfa545d6c459012dbb97854ade46
Henri <52706578+Vanderscycle@users.noreply.github.com>
2021-11-10 12:10:21 -0800

    Update lua.lua

--- lunarvim/lunarvim/site/after/ftplugin/lua.lua
+++ lunarvim/lunarvim/site/after/ftplugin/lua.lua
@@ -1,6 +1,6 @@
 require("lvim.lsp.manager").setup("sumneko_lua")
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup({{exe = "lua-format", filetypes = {"lua"} }})
+formatters.setup({{exe = "stylua", filetypes = {"lua"} }})
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup({{exe = "selene", filetypes = {"lua"} }})
