238085115a6721f57003673dfdfc42eb5c4e6d75
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-12-10 14:13:52 -0800

    got so tired of typing the same command that I created a reinstall script for vim

---
+++ postInstallScripts/reinstalLvim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf ~/.config/lvim ~/.config/lvim.bak
+rm -rf ~/.local/share/lunarvim
+
+# from the official website
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.sh)
+LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
+# neovim bins
+bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
+# syncing my doots
+bash syncDootsLocal.sh
+
--- postInstallScripts/syncDootsLocal.sh
+++ postInstallScripts/syncDootsLocal.sh
@@ -6,13 +6,13 @@ for DOTFILE in "${StringArray[@]}"; do
     # can't use symbolic link since we want the file
     rsync -auv  "$DIR" ~/$DOTFILE
 done
-
-rsync -auv --progress "$DIR/lvim/" ~/.config/lvim/
+# u does not overide (update)
+rsync -av --progress "$DIR/lvim/" ~/.config/lvim/
 # rsync -auv --progress "/$DIR/lunarvim/" ~/.local/share/lunarvim
 rsync -auv --progress "/$DIR/tmuxinator/" ~/.config/tmuxinator/ 
 rsync -auv --progress "/$DIR/kitty/" ~/.config/kitty/
 
+lvim +PackerSync +q
 lvim +LvimUpdate +q
-lvim +PackerUpdate
 
 # maybe combine both bash files into a single on
