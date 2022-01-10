d09ea22ccfdf2e8c9fdda65bead97578817e36a7
Henri <52706578+Vanderscycle@users.noreply.github.com>
2022-01-10 07:47:31 -0800

    Update reinstalLvim.sh

--- postInstallScripts/reinstalLvim.sh
+++ postInstallScripts/reinstalLvim.sh
@@ -2,10 +2,10 @@
 rm -rf ~/.config/lvim ~/.config/lvim.bak ~/.local/share/lunarvim
 
 # from the official website
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.sh)
+sudo bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.sh)
 LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
 # neovim bins
-bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
+sudo bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
 # syncing my doots
 bash ~/Documents/dotFiles/postInstallScripts/syncDootsLocal.sh
 
