ae220a9311dbae4b91890a57f43d2a4db6b81b02
Henri <52706578+Vanderscycle@users.noreply.github.com>
2022-01-14 10:35:00 -0800

    Update endeavourOSXmonad.sh

--- postInstallScripts/endeavourOS/endeavourOSXmonad.sh
+++ postInstallScripts/endeavourOS/endeavourOSXmonad.sh
@@ -293,7 +293,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
 git clone https://github.com/lukechilds/zsh-better-npm-completion ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-better-npm-completion
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+(cd ~/Programs && git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git)
 
 
 # -----------------------------------------------------------------------------
