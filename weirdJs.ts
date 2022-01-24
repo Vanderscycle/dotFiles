5ebcea643ded9f26b922a2c8f3dbc811b10a4855
Henri Vandersleyen <hvandersleyen@gmail.com>
2022-01-24 09:12:37 -0800

    cleaning old dated scripts

--- weirdJs.ts
+++
@@ -1,9 +0,0 @@
-//nested double spread opoerator of an object
-slideOverContent.update((v) => {
-  return {
-    ...v,
-    graphNode: forwardNodes[0].target,
-    graph: { ...v.graph, cameraPosition },
-  };
-});
-
