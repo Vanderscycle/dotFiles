ad42ea031210f0e2355ef30c7e7113b33716a732
Henri Vandersleyen <hvandersleyen@gmail.com>
2022-01-28 15:02:14 -0800

    quicksave

--- postInstallScripts/syncDootsLocal.sh
+++ postInstallScripts/syncDootsLocal.sh
@@ -11,27 +11,9 @@ rg --passthru 'henri' -r $DOOTS_USER_NAME ~/.zshrc > ~/.temp.txt && mv ~/.temp.t
 
 declare -a ConfArray=("lvim","fish","broot","tmuxinator","zellij","tmux", "kitty", "xmobar", "dunst","fontconfig")
 for CONF in "${ConfArray[@]}"; do
-  echo -e "\n=>${CONF}::local -> doots"    
+  echo -e "\n=>${CONF}::doots --> local"    
   rsync -auv --progress ~/Documents/dotFiles/$CONF  ~/.config/$CONF
 done
 rsync -av --progress ~/Documents/dotFiles/.xmonad/ ~/.xmonad/
 
-# rsync -av --progress  ~/.xmonad/ ~/Documents/dotFiles/.xmonad/
-# rsync -av --progress  ~/Documents/dotFiles/lvim/ ~/.config/lvim/
-# rsync -av --progress  ~/Documents/dotFiles/fish/ ~/.config/fish/
-# rsync -av --progress  ~/Documents/dotFiles/broot/ ~/.config/broot/
-# rsync -av --progress  ~/Documents/dotFiles/tmuxinator/ ~/.config/tmuxinator/ 
-# rsync -av --progress  ~/Documents/dotFiles/zellij/ ~/.config/zellij/
-# rsync -av --progress  ~/Documents/dotFiles/tmux/ ~/.config/tmux/
-# rsync -av --progress ~/Documents/dotFiles/kitty/ ~/.config/kitty/
-# rsync -av --progress ~/Documents/dotFiles/xmobar/ ~/.config/xmobar/
-# rsync -av --progress ~/Documents/dotfiles/dunst/ ~/.config/dunst/ 
-# rsync -av --progress  ~/Documents/dotFiles/fontconfig/ ~/.config/fontconfig/
-
-
 lvim +PackerSync +qall
-# lvim +LspInstall svelte tsserver tailwindcss +qall
-
-
-# lvim 
-# maybe combine both bash files into a single on
