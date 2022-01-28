f68bda7915f4327812f7be02cc873ba44251eadf
Henri Vandersleyen <hvandersleyen@gmail.com>
2022-01-28 15:38:48 -0800

    quicksave

--- postInstallScripts/syncDootsLocal.sh
+++ postInstallScripts/syncDootsLocal.sh
@@ -9,7 +9,7 @@ done
 
 rg --passthru 'henri' -r $DOOTS_USER_NAME ~/.zshrc > ~/.temp.txt && mv ~/.temp.txt ~/.zshrc.sh
 
-declare -a ConfArray=("lvim","fish","broot","tmuxinator","zellij","tmux", "kitty", "xmobar", "dunst","fontconfig")
+declare -a ConfArray=("lvim" "fish" "broot" "tmuxinator" "zellij" "tmux" "kitty" "xmobar" "dunst" "fontconfig")
 for CONF in "${ConfArray[@]}"; do
   echo -e "\n=>${CONF}::doots --> local"    
   rsync -auv --progress ~/Documents/dotFiles/$CONF  ~/.config/$CONF
