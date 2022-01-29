53ffdccb2835bb602371978f6c31fd0f09a1b114
Henri Vandersleyen <hvandersleyen@gmail.com>
2022-01-28 21:18:18 -0800

    post clearcache push

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,3 +1,31 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+# https://fishshell.com/docs/current/tutorial.html
+# set -x MyVariable SomeValue === export
+
 end
+
+function zx
+  source ~/.config/fish/config.fish
+end
+
+function save
+  set currentLocation echo $PWD
+  cd ~/Documents/dotFiles/postInstallScripts/
+  bash ./lnSet.sh
+    if test (count $argv) -eq 0
+        git commit
+    else
+        git commit -m "$argv"
+    end
+    git push
+  cd $currentLocation
+end
+
+# function sync
+
+# (cd ~/Documents/dotFiles/postInstallScripts/ &&
+#   git pull &&
+#   bash ./syncDootsLocal.sh)
+# end
+
---
+++ fish/fish/fish/config.fish
@@ -0,0 +1,3 @@
+if status is-interactive
+  # Need to learn how to export/ source and how to create a function
+end
