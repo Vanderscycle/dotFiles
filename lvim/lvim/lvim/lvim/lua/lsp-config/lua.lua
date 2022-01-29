4d5d95c07fe89f1cecb7742a267921dfc5614860
Henri Vandersleyen <hvandersleyen@gmail.com>
2022-01-29 13:19:24 -0800

    found lots of garbage

--- lvim/lvim/lvim/lvim/lua/lsp-config/lua.lua
+++
@@ -1,6 +0,0 @@
-require("lvim.lsp.manager").setup("sumneko_lua")
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup { { exe = "stylua", filetypes = { "lua" } } }
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup { { exe = "selene", filetypes = { "lua" } } }
