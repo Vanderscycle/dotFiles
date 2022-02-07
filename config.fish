727d3872e403d67bb62550703d8290aef95a01ed
Henri Vandersleyen <hvandersleyen@duck.com>
2022-02-07 05:34:08 -0800

    added work in progress script

--- config.fish
+++ config.fish
@@ -88,6 +88,20 @@ function zx
 end
 
 # ~/.zshenv helper func
+# TODO: FIX ME!!!!
+function rga-fzf 
+	set RG_PREFIX "rga --files-with-matches"
+	set file "(
+		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+				--phony -q "$1" \
+				--bind "change:reload:$RG_PREFIX {q}" \
+				--preview-window="70%:wrap"
+	)" &&
+	echo "opening $file" &&
+	xdg-open "$file"
+end
+
 function save
   set currentLocation echo $PWD
   cd ~/Documents/dotFiles/postInstallScripts/
