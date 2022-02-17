cfffd2332950b4b1fa288aa32f447dd12215a7bc
Henri Vandersleyen <hvandersleyen@duck.com>
2022-02-17 09:56:15 -0800

    better reinstall script?

--- postInstallScripts/reinstalLvim.sh
+++ postInstallScripts/reinstalLvim.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-echo -e "=> Purging previous files"
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.sh)
-# from the official website
-echo -e "=> fetching the lvim files from source"
+echo -e 'downloading the latest bins from source' 
+curl -o ~/uninstall.sh https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.sh
 curl -o ~/install.sh https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh
+curl -o ~/binstall.sh https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release
+chmod +x ~/**stall.sh
+
+echo -e "=> Purging previous files"
+sudo bash ~/uninstall.sh
+rm ~/uninstall.sh
+
+eccho -e "=> Installing nvim latest bins"
+sudo bash ~/binstall.sh
+rm ~/binstall.sh
+
+echo -e "=> fetching the lvim files from source"
 export LV_BRANCH=rolling 
 sudo bash ~/install.sh  --no-install-dependencies 
 rm ~/install.sh
-# neovim bins
-echo -e "=> fetching nvim latest binaries"
-sudo bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
-# syncing my doots
+
+
 echo -e "=> syncing doots"
 bash ~/Documents/dotFiles/postInstallScripts/syncDootsLocal.sh
+
 echo -e "=> refreshing dunst"
 bash ~/Documents/dotFiles/dunst/reloadDunst.sh
 
