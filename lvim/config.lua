9943610a2763f97e91028160a9e8a9e413173593
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-03 09:05:41 -0800

    more work being done on the config

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,24 +4,24 @@ require("user.additionalPlugins").config()
 
 -- LSP
 -- =========================================
-require("lsp-config.general")
+-- require("lsp-config.general")
 
 -- Customization
 -- =========================================
-require("user.autocommands")
+-- require("user.autocommands")
 
 -- user specific
 -- =========================================
-require("user.keybindings")
-require("user.todoComments")
-require("user.general")
+-- require("user.keybindings")
+-- require("user.todoComments")
+-- require("user.general")
 
 -- Linter/Formatters
 -- =========================================
 -- require("lsp-config.lua")
 -- require("lsp-config.bash")
-require("lsp-config.rust")
-require("lsp-config.svelte")
+-- require("lsp-config.rust")
+-- require("lsp-config.svelte")
 -- require("lsp-config.typescript")
 -- require("lsp-config.javascript")
 -- require("lsp-config.python")
