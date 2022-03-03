35ce351b96bb41e94961777ded6888dd7ad306e5
Henri Vandersleyen <hvandersleyen@duck.com>
2022-03-03 08:52:06 -0800

    I am understanding lua a bit better in its improt system

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,27 +1,29 @@
--- Imports
+-- Extra plugins
 -- =========================================
-require("plugins.default")
-require("lsp-config.lua")
-require("lsp-config.bash")
--- require("lsp-config.rust")
-require("lsp-config.typescript")
-require("lsp-config.javascript")
-require("lsp-config.python")
-require("lsp-config.markdown")
-require("lsp-config.tailwindcss")
+require("user.additionalPlugins").config()
 
---LSP
-require("lsp-config.general")
--- keybindings
+-- LSP
 -- =========================================
+require("lsp-config.general")
 
 -- Customization
 -- =========================================
 require("user.autocommands")
+
 -- user specific
 -- =========================================
 require("user.keybindings")
 require("user.todoComments")
 require("user.general")
+
 -- Linter/Formatters
 -- =========================================
+-- require("lsp-config.lua")
+-- require("lsp-config.bash")
+require("lsp-config.rust")
+require("lsp-config.svelte")
+-- require("lsp-config.typescript")
+-- require("lsp-config.javascript")
+-- require("lsp-config.python")
+-- require("lsp-config.markdown")
+-- require("lsp-config.tailwindcss")
