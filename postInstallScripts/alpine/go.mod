754c627fb632dcc3c0733ef893e4b48123149a02
Henri Vandersleyen <hvandersleyen@duck.com>
2022-04-05 11:48:37 -0700

    now obtaining all the info aboot the system

--- postInstallScripts/alpine/go.mod
+++ postInstallScripts/alpine/go.mod
@@ -1,3 +1,5 @@
 module alpine
 
 go 1.18
+
+require github.com/zcalusic/sysinfo v0.9.5
--- postInstallScripts/alpine/utils/utils.go
+++ postInstallScripts/alpine/utils/utils.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"encoding/json"
+	// "fmt"
+	"github.com/zcalusic/sysinfo"
 	"log"
+	// "os/user"
 	"runtime"
 )
 
@@ -10,5 +14,20 @@ func OsCheck() {
 		log.Print("Detected: %s; Program not implemented for windows machines", runtime.GOOS)
 	} else {
 		log.Print("Detected: %s", runtime.GOOS)
+		checkLinuxVersion()
 	}
+}
+
+func checkLinuxVersion() {
+
+	var si sysinfo.SysInfo
+
+	si.GetSysInfo()
+
+	data, err := json.MarshalIndent(&si, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(string(data))
 }
