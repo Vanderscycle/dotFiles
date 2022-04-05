ad857b1aaab436d806451e8be3910eb182e46f62
Henri Vandersleyen <hvandersleyen@duck.com>
2022-04-05 14:19:27 -0700

    ready to test in an alpine env. need to package and send

--- postInstallScripts/alpine/install/install.go
+++ postInstallScripts/alpine/install/install.go
@@ -7,6 +7,29 @@ import (
 )
 
 func Installer() {
+	essentialPackages := []string{"vim", "sudo", "fish"}
+	// var essentialPackages []string = []string{"vim", "sudo", "fish"}
+	const pkgManager string = "apk"
+	binary, lookErr := exec.LookPath(pkgManager)
+	if lookErr != nil {
+		panic(lookErr)
+	}
+
+	env := os.Environ()
+
+	for _, pkg := range essentialPackages {
+		args := []string{pkgManager, "add", pkg}
+		execErr := syscall.Exec(binary, args, env)
+		if execErr != nil {
+			panic(execErr)
+		}
+
+	}
+
+	lsExample()
+}
+
+func lsExample() {
 	binary, lookErr := exec.LookPath("ls")
 	if lookErr != nil {
 		panic(lookErr)
