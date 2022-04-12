2f124bb01c9b5aff2175b9395207700360f57ba4
Henri Vandersleyen <hvandersleyen@duck.com>
2022-04-12 09:20:37 -0700

    learned about contanetation of string and appending slices/arrays

--- postInstallScripts/endeavourOS/install/install.go
+++ postInstallScripts/endeavourOS/install/install.go
@@ -10,7 +10,7 @@ import (
 )
 
 //INFO: https://stackoverflow.com/questions/17555857/go-unpacking-array-as-arguments
-var EssentialPackages = []string{"neofetch"}
+var EssentialPackages = []string{"neofetch", "sed"}
 var PkgManagers = [2]string{"pacman", "yay"}
 var EverPresentArgs = []string{"-S", "--needed", "--noconfirm"}
 
@@ -25,17 +25,22 @@ func Installer(pkgs ...string) {
 
 	// pkgManagers := new(PkgManagers).Init()
 	//WIP:testing for pacman only
-	_, lookErr := exec.LookPath(PkgManagers[0])
+	_, lookErr := exec.LookPath(pkgs[0])
 	if lookErr != nil {
 		panic(lookErr)
 	}
 
 	// env := os.Environ()
 
+	//slices -> string (concatenation)
 	flags := strings.Join(EverPresentArgs, " ")
 	pkgStr := strings.Join(pkgs, " ")
-	args := fmt.Sprintf("%s %s %s", PkgManagers[0], flags, pkgStr)
+	args := fmt.Sprintf("%s %s %s", pkgs[0], flags, pkgStr)
 	log.Println(args)
+
+	v := append([]string{PkgManagers[0]}, EverPresentArgs...)
+	v = append(v, pkgs...)
+	log.Print(v)
 
 	// execErr := syscall.Exec(binary, "", env)
 	// if execErr != nil {
