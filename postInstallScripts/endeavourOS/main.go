ee28853f2a1d95361d66861925168fa9b6516ddf
Henri Vandersleyen <hvandersleyen@duck.com>
2022-04-13 08:52:27 -0700

    now have a sudo func

--- postInstallScripts/endeavourOS/main.go
+++ postInstallScripts/endeavourOS/main.go
@@ -9,18 +9,31 @@ import (
 var PkgManagers = []string{"pacman", "yay"}
 var EssentialPackages = []string{"neofetch", "sed"}
 var ArgsInstall = []string{"-S", "--needed", "--noconfirm"}
+var amazingTUI = []string{}
 
 func main() {
 	// we could go function install several packages: e.g. cli,tui,gui,etc...
 	//Installing tui
-	command := append([]string{"sudo"}, PkgManagers[0]) // in this example it requires sudo
-	command = append(command, ArgsInstall...)
-	command = append(command, EssentialPackages...)
-	log.Printf("[INFO]%s", strings.Join(command, " "))
+	// command := append([]string{"sudo"}, PkgManagers[0]) // in this example it requires sudo
+	// command = append(command, ArgsInstall...)
+	// command = append(command, EssentialPackages...)
+	// log.Printf("[INFO]%s", strings.Join(command, " "))
+	command := sudo(PkgManagers[0], ArgsInstall, EssentialPackages, false)
 
 	err := install.Bash(PkgManagers[0], command)
 	if err != nil {
 		log.Println("error format")
 		panic(err)
 	}
+}
+
+func sudo(prg string, flags []string, cmds []string, debug bool) []string {
+	s := append([]string{"sudo"}, []string{prg}...)
+	s = append(s, flags...)
+	s = append(s, cmds...)
+	if debug {
+		log.Printf("[INFO]%s", strings.Join(s, " "))
+
+	}
+	return s
 }
