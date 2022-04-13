596201b21782985881927912d5e7ebd037cf9119
Henri Vandersleyen <hvandersleyen@duck.com>
2022-04-13 10:24:56 -0700

    tested correct

--- postInstallScripts/endeavourOS/main.go
+++ postInstallScripts/endeavourOS/main.go
@@ -9,16 +9,21 @@ import (
 var PkgManagers = []string{"pacman", "yay"}
 var EssentialPackages = []string{"neofetch", "sed"}
 var ArgsInstall = []string{"-S", "--needed", "--noconfirm"}
-var amazingTUI = []string{""}
+var amazingTUI = []string{"curl wget"}
 var lvimPkgs = []string{"neovim"}
 
 func main() {
 	//syncing doots
-		URL: "https://github.com/Vanderscycle/dot-config.git ",
+	// URL: "https://github.com/Vanderscycle/dot-config.git ",
 
-
-	err := bash.General("ls", []string{"-l", "-a"}, nil, true)
+	err := bash.Update(PkgManagers[1], true)
 	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	err2 := bash.General("ls", []string{"-l", "-a"}, nil, true)
+	if err2 != nil {
 		log.Fatal(err)
 		os.Exit(2)
 	}
