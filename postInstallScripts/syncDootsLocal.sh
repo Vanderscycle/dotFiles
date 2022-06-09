00fa4314ad6612711d2e69194ec3d217f4fdf6cd
Henri Vandersleyen <Henri-Vandersleyen@protonmail.com>
2022-06-09 08:32:02 -0700

    added neofetch config

--- postInstallScripts/syncDootsLocal.sh
+++ postInstallScripts/syncDootsLocal.sh
@@ -12,7 +12,7 @@ done
 
 rg --passthru 'henri' -r $DOOTS_USER_NAME ~/.zshrc > ~/.temp.txt && mv ~/.temp.txt ~/.zshrc.sh
 
-declare -a ConfArray=("lvim" "broot" "tmuxinator" "zellij" "tmux" "kitty" "xmobar" "dunst" "fontconfig" "ag" "qutebrowser" "mimeapps.list" "bat" "rg")
+declare -a ConfArray=("lvim" "broot" "tmuxinator" "zellij" "tmux" "kitty" "xmobar" "dunst" "fontconfig" "ag" "qutebrowser" "mimeapps.list" "bat" "rg" "neofetch")
 for CONF in "${ConfArray[@]}"; do
   echo -e "\n=>${CONF}::doots --> local"    
   rsync -av --progress ~/Documents/dotFiles/.config/$CONF  ~/.config/
