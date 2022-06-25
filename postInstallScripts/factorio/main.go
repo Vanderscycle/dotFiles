22fa7461b7eaf1a4a0e64a9f7a07ea1a345645c1
Henri Vandersleyen <Henri-Vandersleyen@protonmail.com>
2022-06-25 08:23:06 -0700

    Everything that is not saved will be lost

--- postInstallScripts/factorio/main.go
+++ postInstallScripts/factorio/main.go
@@ -6,6 +6,8 @@ import (
 	// "reflect" //check the type
 )
 
+var orderInstall = []string{"Kubectl", "Test"}
+
 func main() {
 	json, err := alpine.ParseOrder("routines/k8s.json", false)
 	if err != nil {
@@ -14,7 +16,10 @@ func main() {
 	// log.Print(len(json.Test[0].Args))
 	// json2, _ := alpine.UnstructuredParseOrder("routines/k8s.json" , false)
 	// log.Println(json2)
-	log.Println(len(json.Test))
+	for _, block := range orderInstall {
+		log.Print(block)
+	}
+	log.Print(json)
 	for _, v := range json.Test {
 		err = alpine.Apk(v.Cmd, v.Args)
 		if err != nil {
