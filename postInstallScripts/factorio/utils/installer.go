6dc7d304cd8283a0ab297de677d10f1699caf3a4
Henri Vandersleyen <Henri-Vandersleyen@protonmail.com>
2022-06-28 09:50:06 -0700

    started kubernetes work

--- postInstallScripts/factorio/utils/installer.go
+++ postInstallScripts/factorio/utils/installer.go
@@ -7,6 +7,7 @@ import (
 )
 
 func Installer() error {
+	// if the cmd failed check if installed. if yes crash else ask to install it
 	log.Println(runtime.GOOS, runtime.GOARCH)
 	if runtime.GOOS == "linux" {
 		log.Println("Hello from Linux")
