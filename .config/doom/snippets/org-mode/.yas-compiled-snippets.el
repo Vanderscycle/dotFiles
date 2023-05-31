c5f97aa3b6624bd9ffa4215f888829b2b027f087
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2023-05-30 20:41:23 -0700

    Everything that is not saved will be lost

--- .config/doom/snippets/org-mode/.yas-compiled-snippets.el
+++ .config/doom/snippets/org-mode/.yas-compiled-snippets.el
@@ -2,8 +2,8 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'org-mode
-                     '(("section" "** ${1:section}\n*** Table of Content\n    1. +[[$2]]\n    *** $2" "section" t nil nil "/home/henri/.config/doom/snippets/org-mode/section" nil "t")
-                       ("chapter" "* ${1:chapter}\n** Table of Content\n   1. +[[$2]]\n** $2\n*** ${3:sub-section}\n" "chapter" nil nil nil "/home/henri/.config/doom/snippets/org-mode/chapter" nil nil)))
+                     '(("section" "** ${1:section}\n   - +[[Table of Content]]\n    *** ${2:subsection}" "section" t nil nil "/home/henri/.config/doom/snippets/org-mode/section" nil "t")
+                       ("chapter" "* ${1:chapter}\n** Table of Content\n   1. +[[$2]]\n** $2\n   - +[[Table of Content]]\n*** ${3:sub-section}\n" "chapter" nil nil nil "/home/henri/.config/doom/snippets/org-mode/chapter" nil nil)))
 
 
-;;; Do not edit! File generated at Tue May 30 14:56:03 2023
+;;; Do not edit! File generated at Tue May 30 20:38:25 2023
