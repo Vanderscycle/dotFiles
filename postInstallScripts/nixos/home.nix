a9c3db986f7f83b09d1bf2a1d9d398f9e181309c
Henri <52706578+Vanderscycle@users.noreply.github.com>
2023-06-20 23:34:45 -0400

    added home-manager

---
+++ postInstallScripts/nixos/home.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+{
+  # please change the username & home direcotry to your own
+  home.username = "henri";
+  home.homeDirectory = "/home/henri";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "vanderscycle";
+    userEmail = "henri-vandersleyen@protonmail.com";
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = [
+    pkgs.btop
+  ];
+
+  # enable starship, a beautiful shell prompt
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      aws.disabled = false;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
