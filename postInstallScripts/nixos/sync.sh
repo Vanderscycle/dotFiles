000438a90716477624325cf7737e4b10dc0ad769
vanderscycle <henri-vandersleyen@protonmail.com>
2023-06-23 19:31:00 -0700

    corrected typo

--- postInstallScripts/nixos/sync.sh
+++ postInstallScripts/nixos/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # in rsync (bash?) if the path is followed by a / it only copies the contents of the directory
-declare -a NIX_FOLDERS_PATH=("/etc/nixos" "$HOME/.config/home-manger")
+declare -a NIX_FOLDERS_PATH=("/etc/nixos" "$HOME/.config/home-manager")
 sync(){
     for FOLDER in "${NIX_FOLDERS_PATH}"; do
         rsync -aPv "$FOLDER" .
