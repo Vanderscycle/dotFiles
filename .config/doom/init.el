1ce4b2ffa9f7dd3ef959e522e99c8a2561e84053
vanderscycle <henri-vandersleyen@protonmail.com>
2023-06-27 20:07:34 -0700

    added nix to doom

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -151,7 +151,7 @@
       (lua +lsp)               ; one-based indices? one-based indices
       (markdown +grip)          ; writing docs for people to ignore
       ;;nim               ; python + lisp at the speed of c
-      ;;nix               ; I hereby declare "nix geht mehr!"
+      nix               ; I hereby declare "nix geht mehr!"
       ;;ocaml             ; an objective camel
       (org               ; organize your plain life in plain text
        +pretty
