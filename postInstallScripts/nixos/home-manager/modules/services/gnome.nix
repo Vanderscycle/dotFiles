e9b8a661e6c5cdd02e97a0a71597589577a5da7b
vanderscycle <henri-vandersleyen@protonmail.com>
2023-06-28 11:29:25 -0700

    played with themes

--- postInstallScripts/nixos/home-manager/modules/services/gnome.nix
+++ postInstallScripts/nixos/home-manager/modules/services/gnome.nix
@@ -3,15 +3,31 @@
     # INFO used for lxappearance dark mode theme
     gtk = {
       enable = true;
+    cursorTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+};
       theme = {
-        name = "Materia-dark";
-        package = pkgs.materia-theme;
-      };
+        name = "Adwaita";
+        # name = "Materia-dark";
+        package = pkgs.gnome.adwaita-icon-theme;
+        # package = pkgs.materia-theme;
+          };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
       gtk3 = {
         extraConfig = {
           gtk-application-prefer-dark-theme = true;
         };
       };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
     };
     qt = {
       style = {
