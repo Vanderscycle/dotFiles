e4a5b7519e29c99bbe0f18ade32ba9214d06abff
vanderscycle <henri-vandersleyen@protonmail.com>
2023-07-12 11:29:01 -0700

    one more

--- postInstallScripts/nixos/install.sh
+++ postInstallScripts/nixos/install.sh
@@ -3,6 +3,8 @@
 install() {
 	sudo cp -r nixos/configuration.nix /etc/nixos
 	mkdir /home/henri/.config/home-manager
+	sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+	sudo nix-channel --update
 	sudo cp -r home-manager /home/henri/.config/home-manager
 	nixos-rebuild switch
 }
