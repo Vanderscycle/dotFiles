98ee094f1ccb5cbcc7f36ce4abc23baca8c22ad0
vanderscycle <henri-vandersleyen@protonmail.com>
2023-07-12 11:21:20 -0700

    for laptop install

--- postInstallScripts/nixos/install.sh
+++ postInstallScripts/nixos/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-install(){
-    sudo cp -r nixos/configuration.nix /etc/nixos
-    sudo cp -r home-manager ~/.config/home-manager
-    nixos-rebuild switch
+install() {
+	sudo cp -r nixos/configuration.nix /etc/nixos
+	mkdir ~/.config/home-manager
+	sudo cp -r home-manager ~/.config/home-manager
+	nixos-rebuild switch
 }
 install
