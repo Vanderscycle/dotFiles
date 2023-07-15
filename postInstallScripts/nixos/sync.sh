428db115a6b608121c45abde3909cebafb53a0af
vanderscycle <henri-vandersleyen@protonmail.com>
2023-07-15 09:19:45 -0700

    fixed issue

--- postInstallScripts/nixos/sync.sh
+++ postInstallScripts/nixos/sync.sh
@@ -3,7 +3,7 @@
 sync() {
   git pull
 	sudo rsync -aPv "$HOME"/Documents/dotFiles/nixos/configuration.nix /etc/nixos
-  sudo rsync -aPv "$HOME"/Documents/dotFiles/home-manager/ "$HOME"/.config/home-manager
+  sudo rsync -aPv "$HOME"/Documents/dotFiles/home-manager "$HOME"/.config/home-manager
 	nixos-rebuild switch
 }
 sync
