98c2273ed1a37d1f29f96abdc92a9ae7f3aea021
vanderscycle <henri-vandersleyen@protonmail.com>
2023-07-15 17:30:47 -0700

    small correction

--- postInstallScripts/nixos/sync.sh
+++ postInstallScripts/nixos/sync.sh
@@ -2,8 +2,8 @@
 
 sync() {
   git pull
-	sudo rsync -aPv "$HOME"/Documents/dotFiles/nixos/configuration.nix /etc/nixos
-  sudo rsync -aPv "$HOME"/Documents/dotFiles/home-manager "$HOME"/.config/home-manager
+	sudo rsync -aPv "$HOME"/Documents/dotFiles/postInstallScripts/nixos/configuration.nix /etc/nixos
+  sudo rsync -aPv "$HOME"/Documents/dotFiles/postInstallScripts/nixos/home-manager "$HOME"/.config/home-manager
 	nixos-rebuild switch
 }
 sync
