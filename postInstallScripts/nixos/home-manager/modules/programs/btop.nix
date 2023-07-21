d2649e677fef3725ba4a8ba39146d73acc73e8b5
vanderscycle <henri-vandersleyen@protonmail.com>
2023-07-21 09:40:29 -0700

    fixed btop theme

--- postInstallScripts/nixos/home-manager/modules/programs/btop.nix
+++ postInstallScripts/nixos/home-manager/modules/programs/btop.nix
@@ -1,5 +1,14 @@
 { config, pkgs, ... }:
+let
+  dotfiles_dir = /home/henri/Documents/dotFiles;
+in
 {
+  home = {
+    file = {
+      # theme
+      ".config/btop/themes/catppuccin_mocha.theme".source = "${dotfiles_dir}/.config/bpytop/themes/catppuccin_mocha.theme";
+    };
+  };
   programs.btop = {
     enable = true;
     settings = {
