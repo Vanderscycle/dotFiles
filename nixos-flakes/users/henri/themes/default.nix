1adeab51cbcb844345f1c60177f1e75291d99db1
vanderscycle <henri-vandersleyen@protonmail.com>
2024-01-25 13:09:13 -0800

    small fix

--- nixos-flakes/users/henri/themes/default.nix
+++ nixos-flakes/users/henri/themes/default.nix
@@ -1,7 +1,7 @@
 let
   tokyonight = import ./tokyonight.nix;
-  catppuccin_mocha =I import ./catppuccin_mocha.nix;
+  catppuccin_mocha = import ./catppuccin_mocha.nix;
   # catppuccin_macchiato = import ./catppuccin_macchiato.nix;
 in
 #tokyonight
-catppuccin_macchiato
+catppuccin_mocha
