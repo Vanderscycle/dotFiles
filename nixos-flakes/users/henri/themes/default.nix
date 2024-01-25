92b81b4d3e56ac34dbdbf154864f613b4a9b08d8
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2024-01-25 13:17:09 -0800

    small laptop changes

--- nixos-flakes/users/henri/themes/default.nix
+++ nixos-flakes/users/henri/themes/default.nix
@@ -1,5 +1,5 @@
 let
-  tokyonight = import ./tokyonight.nix;
+  #tokyonight = import ./tokyonight.nix;
   catppuccin_mocha = import ./catppuccin_mocha.nix;
   # catppuccin_macchiato = import ./catppuccin_macchiato.nix;
 in
