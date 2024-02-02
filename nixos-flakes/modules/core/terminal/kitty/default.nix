39b5ccef15a1c10c48747fa1b1764a236fbe7e53
vanderscycle <henri-vandersleyen@protonmail.com>
2024-02-01 18:57:50 -0800

    fixed wezterm

--- nixos-flakes/modules/core/terminal/kitty/default.nix
+++ nixos-flakes/modules/core/terminal/kitty/default.nix
@@ -1,6 +1,5 @@
 { username, home-manager, pkgs, ... }:
 {
-
   home-manager.users.${username} = {
     programs = {
       kitty = {
--- nixos-flakes/modules/core/terminal/wezterm/default.nix
+++ nixos-flakes/modules/core/terminal/wezterm/default.nix
@@ -1,40 +1,42 @@
-{ pkgs, ... }:
+{ username, home-manager, pkgs, ... }:
 {
-  programs = {
-    wezterm = {
-      enable = true;
-      extraConfig = ''
-        return {
-          use_ime = true,
-          xim_im_name = "fcitx",
-          font = wezterm.font_with_fallback({
-                  { family = "JetBrains Mono", weight = "Regular", italic = false },
-          }),          
+  home-manager.users.${username} = {
+    programs = {
+      wezterm = {
+        enable = true;
+        extraConfig = ''
+          return {
+            use_ime = true,
+            xim_im_name = "fcitx",
+            font = wezterm.font_with_fallback({
+                    { family = "JetBrains Mono", weight = "Regular", italic = false },
+            }),          
 
-          font_rules = {
-                  {
-                          intensity = "Normal",
-                          italic = true,
-                          font = wezterm.font_with_fallback({
-                                  { family = "JetBrains Mono", weight = "Regular", italic = false },
-                          }),
-                  },
-                  {
-                          intensity = "Bold",
-                          italic = true,
-                          font = wezterm.font_with_fallback({
-                                  { family = "JetBrains Mono", weight = "Bold", italic = false },
-                          }),
-                  },
-          },
-          font_size = 14.0,
-          tab_bar_at_bottom = true,
-          use_fancy_tab_bar = true,
-          enable_tab_bar = true,
-          hide_tab_bar_if_only_one_tab = true,
-          color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
-        }
-      '';
+            font_rules = {
+                    {
+                            intensity = "Normal",
+                            italic = true,
+                            font = wezterm.font_with_fallback({
+                                    { family = "JetBrains Mono", weight = "Regular", italic = false },
+                            }),
+                    },
+                    {
+                            intensity = "Bold",
+                            italic = true,
+                            font = wezterm.font_with_fallback({
+                                    { family = "JetBrains Mono", weight = "Bold", italic = false },
+                            }),
+                    },
+            },
+            font_size = 14.0,
+            tab_bar_at_bottom = true,
+            use_fancy_tab_bar = true,
+            enable_tab_bar = true,
+            hide_tab_bar_if_only_one_tab = true,
+            color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+          }
+        '';
+      };
     };
   };
 }
