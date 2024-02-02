029a70965c80d3ddb461f660f604ed0631205eae
vanderscycle <henri-vandersleyen@protonmail.com>
2024-02-01 18:56:49 -0800

    one more fix

--- nixos-flakes/modules/core/terminal/kitty/default.nix
+++ nixos-flakes/modules/core/terminal/kitty/default.nix
@@ -1,21 +1,24 @@
-{ pkgs, ... }:
+{ username, home-manager, pkgs, ... }:
 {
-  programs = {
-    kitty = {
-      enable = true;
-      theme = "Catppuccin-Mocha";
-      shellIntegration.enableFishIntegration = true;
-      settings = {
-        allow_remote_control = "yes";
-        # Add other Kitty settings here if needed
-      };
-      environment = {
-        KITTY_LISTEN_ON = "/tmp/mykitty";
-      };
-      font = {
-        size = 14;
-        name = "JetBrainsMono";
-        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+
+  home-manager.users.${username} = {
+    programs = {
+      kitty = {
+        enable = true;
+        theme = "Catppuccin-Mocha";
+        shellIntegration.enableFishIntegration = true;
+        settings = {
+          allow_remote_control = "yes";
+          # Add other Kitty settings here if needed
+        };
+        environment = {
+          KITTY_LISTEN_ON = "/tmp/mykitty";
+        };
+        font = {
+          size = 14;
+          name = "JetBrainsMono";
+          package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        };
       };
     };
   };
