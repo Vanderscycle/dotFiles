bbf352b6b332254c8b0e90aa12ab04a9f5010f1c
vanderscycle <henri-vandersleyen@protonmail.com>
2024-02-09 17:06:12 -0800

    test2

--- rpi-flakes/modules/master/cmdline.nix
+++ rpi-flakes/modules/master/cmdline.nix
@@ -1,35 +1,30 @@
-{ hostname, lib, pkgs, ... }:
+{ hostname lib, pkgs, ... }:
 
 {
   networking = {
     hostName = hostname; # Set the system's host name
-    useDHCP = false; # Disable DHCP
-    interfaces.eth0 = {
-      useDHCP = false;
-      ipv4 = {
-        addresses = [
-          { address = "10.7.1.60"; }
-        ];
-        routes = [
-          { address = "10.7.1.3"; }
-        ];
-      };
-    };
+    useDHCP = false; # Disable DHCP for all interfaces
+    interfaces.eth0.useDHCP = false; # Specifically disable DHCP on eth0
+    interfaces.eth0.ipv4.addresses = [
+      { address = "10.7.1.60"; prefixLength = 24; } # Correctly include prefixLength
+    ];
+    routes = [
+      { address = "default"; via = "10.7.1.3"; } # Correctly define the default gateway
+    ];
   };
 
   boot.kernelParams = [
-    "console=serial0,115200"
-    "console=tty1"
-    "root=/dev/mmcblk0p2"
-    "rootfstype=ext4"
-    "elevator=deadline"
-    "fsck.repair=yes"
-    "rootwait"
-    "cgroup_memory=1"
-    "cgroup_enable=memory"
+    "console=serial0,115200",
+    "console=tty1",
+    "root=/dev/mmcblk0p2",
+    "rootfstype=ext4",
+    "elevator=deadline",
+    "fsck.repair=yes",
+    "rootwait",
+    "cgroup_memory=1",
+    "cgroup_enable=memory",
   ];
 
-  # Ensure the system is configured for the required cgroups
   boot.kernel.sysctl = {
     "kernel.cgroup.memory" = 1;
     "kernel.cgroup.enable" = "memory";
