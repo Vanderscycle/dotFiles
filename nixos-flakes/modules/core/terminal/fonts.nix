03b232e17ee7f514ba95601b7e413a9210db6cfc
vanderscycle <henri-vandersleyen@protonmail.com>
2024-04-10 09:17:46 -0700

    update

--- nixos-flakes/modules/core/terminal/fonts.nix
+++ nixos-flakes/modules/core/terminal/fonts.nix
@@ -5,5 +5,27 @@
   fonts.packages = with pkgs; [
     jetbrains-mono
     nerd-font-patcher
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-extra
   ];
 }
+# TODO: fix chinese characters
+# {
+#   fonts = {
+#     packages = with pkgs; [
+#       nerdfonts
+#       noto-fonts
+#       noto-fonts-cjk
+#       noto-fonts-emoji
+#       roboto
+#     ];
+#     fontconfig.defaultFonts = {
+#       serif = [ "Roboto Serif" "Noto Color Emoji" ];
+#       sansSerif = [ "Roboto" "Noto Color Emoji" ];
+#       emoji = [ "Noto Color Emoji" ];
+#     };
+#   };
+# }
--- nixos-flakes/users/henri/programs/default.nix
+++ nixos-flakes/users/henri/programs/default.nix
@@ -21,6 +21,24 @@
     defaultApplications = {
       "application/pdf" = [ "zathura.desktop" ];
       "application/json" = [ "vim.desktop" ]; # You'll need a .desktop file for Vim or your preferred editor
+    # Video formats
+    "video/mp4" = [ "vlc.desktop" ];
+    "video/mpeg" = [ "vlc.desktop" ];
+    "video/x-matroska" = [ "vlc.desktop" ];
+    "video/quicktime" = [ "vlc.desktop" ];
+    "video/x-msvideo" = [ "vlc.desktop" ]; # AVI
+    "video/webm" = [ "vlc.desktop" ];
+    # Directories
+    "inode/directory" = [ "thunar.desktop" ];
+    # Images
+    # "image/jpeg" = [ "feh.desktop" ];
+    # "image/png" = [ "feh.desktop" ];
+    # "image/gif" = [ "feh.desktop" ];
+    # audio
+    "audio/mpeg" = [ "vlc.desktop" ];
+    "audio/flac" = [ "vlc.desktop" ];
+
+
     };
   };
 }
