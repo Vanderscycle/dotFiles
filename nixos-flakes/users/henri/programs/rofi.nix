94903f1682dac16597bb51ad7caf5855b28aeff4
vanderscycle <henri-vandersleyen@protonmail.com>
2024-04-24 08:40:50 -0700

    updated rofi

--- nixos-flakes/users/henri/programs/rofi.nix
+++ nixos-flakes/users/henri/programs/rofi.nix
@@ -1,17 +1,25 @@
-{ username, home-manager, pkgs, ... }:
+{
+  username,
+  home-manager,
+  pkgs,
+  ...
+}:
 let
   dotfiles_dir = /home/henri/Documents/dotFiles;
 in
 {
   home-manager.users.${username} = {
     home = {
-      file.".config/rofi" = {
-        source = "${dotfiles_dir}/.config/rofi";
-        recursive = true;
-      };
-      packages = with pkgs; [
-        rofi
-      ];
+      # file.".config/rofi" = {
+      #   source = "${dotfiles_dir}/.config/rofi";
+      #   recursive = true;
     };
+    packages.rofi = {
+      enable = true;
+      catppuccin.enable = true;
+    };
+    # packages = with pkgs; [
+    #   rofi
+    # ];
   };
 }
--- nixos-flakes/users/henri/programs/starship.nix
+++ nixos-flakes/users/henri/programs/starship.nix
@@ -5,18 +5,21 @@
       enable = true;
       catppuccin.enable = true;
       enableFishIntegration = true;
+      # https://starship.rs/config/
       settings = {
         aws = {
           format = "on [$symbol($profile )(\($region\) )]($style)";
           style = "bold blue";
           symbol = "🅰 ";
         };
-
         kubernetes = {
           format = "on [⛵ $context ](dimmed green) ";
           disabled = false;
         };
 
+        sudo = {
+          disabled = false;
+        };
         terraform = {
           format = "[💨 $version$workspace]($style) ";
         };
@@ -29,9 +32,7 @@
             style = "bold red";
           };
         };
-
         format = "$all"; # Remove this line to disable the default prompt format
-        # palette = "catppuccin_${flavour}";
       };
     };
   };
