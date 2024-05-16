7f0a070c887bea3a7b77cee6f3db9645617100b1
vanderscycle <henri-vandersleyen@protonmail.com>
2024-05-15 21:01:20 -0700

    update

--- nixos-flakes/modules/core/security/sops.nix
+++ nixos-flakes/modules/core/security/sops.nix
@@ -1,20 +1,24 @@
-{ pkgs, inputs, config, username,... }:
+{
+  pkgs,
+  inputs,
+  config,
+  username,
+  ...
+}:
 
 {
 
-  imports =
-    [
-      inputs.sops-nix.nixosModules.sops
-    ];
+  imports = [ inputs.sops-nix.nixosModules.sops ];
 
   sops.defaultSopsFile = "/home/${username}/Documents/dotFiles/nixos-flakes/secrets/secrets.yaml";
+  # sops.defaultSopsFile = "./secrets/secrets.yaml";
   sops.defaultSopsFormat = "yaml";
-  
+
   sops.age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
 
   # map them according to the file structure
-  sops.secrets.example-key = { };
-  sops.secrets."myservice/my_subdir/my_secret" = {
-    owner = "sometestservice";
-    };
+  # sops.secrets.example-key = { };
+  # sops.secrets."myservice/my_subdir/my_secret" = {
+  #   owner = "sometestservice";
+  # };
 }
