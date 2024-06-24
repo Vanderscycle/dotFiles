2298dffa9972b771eac0d14243deeeeeaa42d702
vanderscycle <henri-vandersleyen@protonmail.com>
2024-06-23 23:14:40 -0700

    tru-nas is working

--- nixos-flakes/hosts/desktop/fcstab.nix
+++ nixos-flakes/hosts/desktop/fcstab.nix
@@ -12,6 +12,18 @@
       "compress=zstd"
     ];
   };
+  fileSystems."/mnt/rice" = {
+    device = "//192.168.1.157/rice";
+    fsType = "cifs";
+    options = [
+      "credentials=/root/smbcreds_fam"
+      "dir_mode=0770"
+      "file_mode=0770"
+      "uid=1000"
+      "gid=1000"
+      "defaults"
+    ];
+  };
   fileSystems."/mnt/prox-share" = {
     device = "//192.168.1.157/test-nas";
     fsType = "cifs";
