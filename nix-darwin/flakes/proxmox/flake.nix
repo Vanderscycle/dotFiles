75680d485a16f6e70c7e4704bcfbe76b41e21ed2
vanderscycle <henri-vandersleyen@protonmail.com>
2024-12-18 21:25:50 -0800

    update

--- nix-darwin/flakes/proxmox/flake.nix
+++ nix-darwin/flakes/proxmox/flake.nix
@@ -61,6 +61,12 @@
             proxmox-nixos.nixosModules.proxmox-ve
             (
               { pkgs, lib, ... }:
+              let
+                minimalIso = pkgs.fetchurl {
+                  url = "https://releases.nixos.org/nixos/24.05/nixos-24.05.7139.bcba2fbf6963/nixos-minimal-24.05.7139.bcba2fbf6963-x86_64-linux.iso";
+                  hash = "sha256-plre/mIHdIgU4xWU+9xErP+L4i460ZbcKq8iy2n4HT8=";
+                };
+              in
               {
                 imports = [
                   ../../users/proxmox/configuration.nix
@@ -70,6 +76,36 @@
                 services.proxmox-ve = {
                   enable = true;
                   ipAddress = "192.168.1.168";
+                  ceph = {
+                    enable = true;
+                    mgr.enable = true;
+                    mon.enable = true;
+                    osd = {
+                      enable = true;
+                      daemons = [ "1" ];
+                    };
+                  };
+                  # vms
+                  vms = {
+                    node1 = {
+                      vmid = 100;
+                      memory = 4096;
+                      cores = 4;
+                      sockets = 2;
+                      kvm = true
+                      scsi = [ { file = "local:16"; } ];
+                      cdrom = "local:iso/minimal.iso";
+                    };
+                    node2 = {
+                      vmid = 101;
+                      memory = 4096;
+                      cores = 4;
+                      sockets = 2;
+                      kvm = true
+                      scsi = [ { file = "local:32"; } ];
+                      cdrom = "local:iso/minimal.iso";
+                    };
+                  };
                 };
 
                 networking.interfaces.enp10s0 = {
@@ -82,6 +118,13 @@
                 };
 
                 users.users.root = {
+                  password = "mypassword";
+                  initialPassword = null;
+                  hashedPassword = null;
+                  hashedPasswordFile = null;
+                };
+
+                users.users.proxmox= {
                   password = "mypassword";
                   initialPassword = null;
                   hashedPassword = null;
