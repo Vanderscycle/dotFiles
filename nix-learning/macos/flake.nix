b950ad991eb47f4f9374cacd3d9a867991ea71ce
vanderscycle <henri-vandersleyen@protonmail.com>
2025-05-01 10:25:50 -0700

    saving prior testing

--- nix-learning/macos/flake.nix
+++ nix-learning/macos/flake.nix
@@ -7,37 +7,54 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.vim
+          ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          homebrew = {
+            enable = true;
+            casks = [
+              "rectangle"
+            ];
+            brews = [
+              "dive"
+            ];
+          };
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
--- nix-learning/rocky-linux/flake.nix
+++ nix-learning/rocky-linux/flake.nix
@@ -1,15 +1,74 @@
 {
-  description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # Package to create the system user (run as root)
+      packages.${system}.create-user-script = pkgs.writeShellScriptBin "create-user" ''
+        #!/bin/sh
+        useradd -m -s ${pkgs.bash}/bin/bash rocky || echo "User exists"
+        usermod -aG nix-users rocky
+        echo "User 'rocky' created. Run 'passwd rocky' to set password"
+      '';
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      # Home Manager configurations
+      homeConfigurations = {
+        # Root user configuration (optional)
+        "root" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            {
+              home.username = "root";
+              home.homeDirectory = "/root";
+              home.stateVersion = "24.11";
+              home.packages = with pkgs; [
+                htop
+                neovim
+              ];
+            }
+          ];
+        };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        # rocky user configuration
+        "rocky" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            {
+              home.username = "rocky";
+              home.homeDirectory = "/home/rocky";
+              home.stateVersion = "24.11";
+              home.packages = with pkgs; [
+                git
+                tmux
+                starship
+              ];
 
-  };
+              programs.bash = {
+                enable = true;
+                shellAliases = {
+                  ll = "ls -l";
+                  rebuild = "home-manager switch --flake .#newuser";
+                };
+              };
+
+              home.file.".config/starship.toml".text = ''
+                add_newline = false
+              '';
+            }
+          ]
+        };
+      };
+    };
 }
